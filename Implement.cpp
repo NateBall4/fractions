80bef1e853ed0faeb47da65e7187ea6c1cc4b502
NateBall4 <43415745+NateBall4@users.noreply.github.com>
2019-04-08 14:25:17 -0400

    Create Implement.cpp

---
+++ Implement.cpp
@@ -0,0 +1,41 @@
+#include "Fraction.h"
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+void fraction::getNumerator(int numerator) {
+	cout << "please enter a numerator for the fraction: ";
+	cin >> numerator;
+
+	while (cin.fail() == true) {
+		cout << "invalid input";
+		cout << "please enter a numerator for the fraction: ";
+		cin >> numerator;
+	}
+
+	this->numerator = numerator;
+}
+
+void fraction::getDenominator(int denominator) {
+	cout << "please enter a Denominator for the fraction: ";
+	cin >> denominator;
+
+	while (cin.fail() == true || denominator < 0) {
+		cout << "invalid input";
+		cout << "please enter a Denominator for the fraction: ";
+		cin >> denominator;
+	}
+
+	this->denominator = denominator;
+}
+
+fraction fraction::operator+(fraction addMe) {
+
+
+}
+
+fraction fraction::operator-(fraction subtractMe) {
+
+
+}
